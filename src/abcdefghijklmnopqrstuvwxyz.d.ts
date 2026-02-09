08a5e3f4d6850da2913d9df1fc7f7575a1fe2b60
Max Duval <max.duval@hometrack.com>
2026-02-09 10:28:57 +0000

    feat: types and documentation

---
+++ src/abcdefghijklmnopqrstuvwxyz.d.ts
@@ -0,0 +1,6 @@
+/**
+ * A single lower-case 26-character string containing the modern English alphabet,
+ * suitable for using as-is, or for safely interpolating, without the need for escaping,
+ * into nearly any context.
+ */
+declare const abcdefghijklmnopqrstuvwxyz = 'abcdefghijklmnopqrstuvwxyz'
